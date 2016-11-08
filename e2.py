f66092cad9ce2018716f90726ce573767f027dde
ryanthiggins <ryanthiggins@uchicago.edu>
2016-11-08 13:48:11 -0600

    Add files via upload

--- e2.py
+++ e2.py
@@ -17,24 +17,3 @@ for line in open("ELECTION_ID"):
     file_name = year + ".csv"
     with open(file_name, "w") as out:
         out.write(soup)
-
-
-#with open("ELECTION_ID", 'r') as f:
-#    for line in f:
-#        info = line.split()
-#        year = line.split()[0]
-#        codes = line.split()[1]
-#
-#        year = election_years.append(info[0])
-#        election_ids.append(info[1])
-#
-#download_site = "http://historical.elections.virginia.gov/elections/download/{}/precincts_include:0/"
-
-
-#for codes in election_ids:
-#        full_url = download_site.format(codes)
-#        req = requests.get(full_url)
-#        soup = BeautifulSoup(req.text, 'lxml')
-#        file_name = year + ".csv"
-#        with open(file_name, "w") as out:
-#            out.write(req.text)
